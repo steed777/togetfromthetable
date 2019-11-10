2c02bfe313e5c54a9301c54d692cb7c26c630c90
steed777 <steed777@yandex.ru>
2019-11-10 20:08:23 +0300

    add setString()

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,19 +5,19 @@ private static final String url =
         "jdbc:mysql://localhost:3306/books?user=root&password=alberto&ssl=true";
 private static final String url2 =
         "jdbc:mysql://localhost:3306/books3?user=root&password=alberto&ssl=true";
-    public static void main(String[] args) {
-try {
+    public static void main(String[] args) throws SQLException{
+
     Connection connection = DriverManager.getConnection(url);
     Statement statement = connection.createStatement();
     ResultSet resultSet = statement.executeQuery("select * from books");
+    Connection connection1 = DriverManager.getConnection(url2);
 
-Connection connection1 = DriverManager.getConnection(url2);
-while (resultSet.next()) {
+    while (resultSet.next()) {
     PreparedStatement preparedStatement = connection1.prepareStatement
-            ("insert into books3(books_title, books_author) values (? , ?)");
-}
-}catch (SQLException e){
-
+            ("INSERT INTO books3(books_title, books_author) values (?, ?)");
+    preparedStatement.setString(1, "books_title");
+    preparedStatement.setString(2, "books_author");
+    preparedStatement.execute();
 }
     }
 }
