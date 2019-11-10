282dcdf77ddad80c0647b198a23234ae628dc815
steed777 <steed777@yandex.ru>
2019-11-10 22:09:46 +0300

    add setString()

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,21 +3,23 @@ import java.sql.*;
 public class Main {
 private static final String url =
         "jdbc:mysql://localhost:3306/books?user=root&password=alberto&ssl=true";
-private static final String url2 =
-        "jdbc:mysql://localhost:3306/books3?user=root&password=alberto&ssl=true";
+//private static final String url2 =
+       // "jdbc:mysql://localhost:3306/books?user=root&password=alberto&ssl=true";
+
     public static void main(String[] args) throws SQLException{
 
-    Connection connection = DriverManager.getConnection(url);
+        Connection connection = DriverManager.getConnection(url);
     Statement statement = connection.createStatement();
     ResultSet resultSet = statement.executeQuery("select * from books");
-    Connection connection1 = DriverManager.getConnection(url2);
+  //  Connection connection1 = DriverManager.getConnection(url2);
 
     while (resultSet.next()) {
-    PreparedStatement preparedStatement = connection1.prepareStatement
-            ("INSERT INTO books3(books_title, books_author) values (?, ?)");
-    preparedStatement.setString(1, "books_title");
-    preparedStatement.setString(2, "books_author");
-    preparedStatement.execute();
+    PreparedStatement preparedStatement = connection.prepareStatement
+            ("INSERT INTO book3(id, books_title, books_author) values (?, ?, ?)");
+    preparedStatement.setInt(1, resultSet.getInt("id"));
+    preparedStatement.setString(2, resultSet.getString("books_title"));
+    preparedStatement.setString(3, resultSet.getString("books_author"));
+    preparedStatement.executeUpdate();
 }
     }
 }
